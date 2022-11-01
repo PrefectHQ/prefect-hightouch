16503e2cfc6e97d9c0680d05f654fa012799770a
ahuang11 <andrew.h@prefect.io>
2022-11-01 16:15:04 -0700

    Add tests

--- tests/test_credentials.py
+++ tests/test_credentials.py
@@ -5,6 +5,5 @@ from prefect_hightouch.api_client.client import AuthenticatedClient
 def test_hightouch_credentials_get_client():
     client = HightouchCredentials(token="token_value").get_client()
     assert isinstance(client, AuthenticatedClient)
-    assert client.token == "token_value"
     assert client.auth_header_name == "Authorization"
-    assert client.prefix == "Bearer"
+    assert client.token == "token_value"
