67759160db90bb20e3921aefa3cf2ddc745d3e49
ahuang11 <andrew.h@prefect.io>
2022-11-01 12:48:48 -0700

    Override block logo

--- prefect_hightouch/credentials/__init__.py
+++ prefect_hightouch/credentials/__init__.py
@@ -3,3 +3,5 @@
 # below, as to override defaults or expose other imports.
 
 from .generated import *  # noqa: F401, F403
+
+HightouchCredentials._logo_url = "https://images.ctfassets.net/gm98wzqotmnx/46tBvip09DRXFuFQBm6mHW/9c03ce91d396006a97d5efe0f718e4cc/hightouch.png?h=250"  # noqa: F405, E501
