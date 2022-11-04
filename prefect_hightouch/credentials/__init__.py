de376e7dfdc9226893859119cbb0a17a82fbf851
ahuang11 <andrew.h@prefect.io>
2022-11-04 10:14:58 -0700

    Remove extraneous f405

--- prefect_hightouch/credentials/__init__.py
+++ prefect_hightouch/credentials/__init__.py
@@ -5,7 +5,7 @@
 from .generated import *  # noqa: F401, F403 isort: skip
 
 HightouchCredentials._logo_url = "https://images.ctfassets.net/gm98wzqotmnx/46tBvip09DRXFuFQBm6mHW/9c03ce91d396006a97d5efe0f718e4cc/hightouch.png?h=250"  # noqa: F405, E501
-HightouchCredentials.get_client.__doc__ = """  # F405
+HightouchCredentials.get_client.__doc__ = """
 Gets a Hightouch REST API Authenticated Client.
 
 Returns:
